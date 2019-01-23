aa332a49737ea210b19500a2ea96cc1a234361b6
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-01-23 11:24:17 +0200

    Few unit tests were added

--- src/app/_Pages/form-builder/form-builder.component.spec.ts
+++ src/app/_Pages/form-builder/form-builder.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormBuilderComponent } from './form-builder.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('FormBuilderComponent', () => {
   let component: FormBuilderComponent;
   let fixture: ComponentFixture<FormBuilderComponent>;
+  let componentFB: FormBuilderComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +18,23 @@ describe('FormBuilderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FormBuilderComponent);
     component = fixture.componentInstance;
+    componentFB = new FormBuilderComponent(new FormBuilder);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create a form with 3 controls', () => {
+    expect(componentFB.form.contains('name')).toBeTruthy;
+    expect(componentFB.form.contains('contact')).toBeTruthy;
+    expect(componentFB.form.contains('topics')).toBeTruthy;
+  });
+
+  it('name control is required', () => {
+    let control = componentFB.form.get('name');
+    control.setValue('');
+    expect(control.valid).toBeFalsy();
   });
 });
--- src/app/_Services/auth.service.spec.ts
+++ src/app/_Services/auth.service.spec.ts
@@ -1,12 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { HttpEvent, HttpResponse, HttpEventType } from '@angular/common/http';
+import { nextContext } from '@angular/core/src/render3';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AuthService;
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AuthService]
+  }));
+
+  beforeEach(() =>{
+    service = new AuthService(null);
+  })
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
+  });
+
+  it('auth token should be saved if body was returned', () => {
+
+    let httpResp = new HttpResponse({ 
+      status: 200, 
+      body: { token: 'testtoken' 
+    }});
+
+    spyOn(service, 'login').and.callFake(t =>{
+      
+    })
+
+    //second mock approach
+    /*spyOn(service, 'login').and.returnValue({
+
+    })*/
   });
 });
